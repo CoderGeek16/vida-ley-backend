260bedef3f7eb4f40c737866377466021e2c13ac
Hugo Martin De la Puente Solorzano <HDelaPuente@tramarsa.com.pe>
2026-04-26 16:05:18 -0500

    fix port render

--- server.js
+++ server.js
@@ -243,6 +243,8 @@ app.get("/admin/total-beneficiarios", checkAdmin, async (req, res) => {
 });
 
 // ===============================
-app.listen(3000, () => {
-  console.log("Servidor corriendo en 3000 🚀");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor corriendo en " + PORT + " 🚀");
 });
