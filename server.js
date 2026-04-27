76f9722b716c52d2898808aa65739745c038f9a1
Hugo Martin De la Puente Solorzano <HDelaPuente@tramarsa.com.pe>
2026-04-26 23:09:08 -0500

    update cors dominio final

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 const allowedOrigins = [
   "http://127.0.0.1:5500",
   "http://localhost:5500",
-  "https://lucent-unicorn-1b3310.netlify.app"
+  "https://registrovidaley.netlify.app"
 ];
 
 app.use(cors({
