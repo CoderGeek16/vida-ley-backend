bc450b9591a4903d7532fe2bd03b1dcb06f2ea03
Hugo Martin De la Puente Solorzano <hdelapuente@tramarsa.com.pe>
2026-05-06 00:53:25 -0500

    Fix login

--- generarPassword.js
+++ generarPassword.js
@@ -1,10 +1,9 @@
 const bcrypt = require("bcrypt");
 
 async function generar(){
-const passwordPlano = "Wishmaster@3103";
-const hash = await bcrypt.hash(passwordPlano, 10);
-console.log("HASH:", hash);
+  const password = "Wishmaster3103";
+  const hash = await bcrypt.hash(password, 10);
+  console.log(hash);
 }
 
 generar();
-
