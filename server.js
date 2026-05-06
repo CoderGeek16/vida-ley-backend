6989df871117ddf38d1eece01114e3c2652bdc53
Hugo Martin De la Puente Solorzano <hdelapuente@tramarsa.com.pe>
2026-05-06 00:43:41 -0500

    Fix CORS para producción

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ process.env.SUPABASE_SERVICE_ROLE_KEY
 app.use(helmet());
 
 app.use(cors({
-origin: ["http://127.0.0.1:5500", "http://localhost:5500", "https://registrovidaley.netlify.app"],
+origin: true,
 credentials: true
 }));
 
